8c332eb759a2dbf40c7bb3e15c1e0605575f6060
Mahad Ahmad <mahadahmad921@gmail.com>
2024-03-08 20:04:18 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Big o
+
+// Big o notation is mathematically notation that describes the limiting behavior of
+// a function when the argument tends towards a particular value or infinity.
+// We use Big o  to describe the performance of a algorithm. o(n)
+// Certain operations can be more or less costly depending on which data structure we
+// use e.g: accessing an array element using index is very fast but if array is resized
+// this will get costly as size of input grows very large then we can use LINKED LIST
+// that can grow or shrink very quickly but accessing that using index is slow.

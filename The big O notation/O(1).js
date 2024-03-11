21d8965fada6716aa7ec6831363791d0814731a2
Mahad Ahmad <mahadahmad921@gmail.com>
2024-03-11 12:04:15 +0000

    time complexity O(1) added

---
+++ The big O notation/O(1).js
@@ -0,0 +1,20 @@
+// O(1)
+
+const log = (arr) => {
+  console.log(arr[0]);
+};
+
+// Here we are consoling the fisrt item of the array no matter how big the array is it
+// will print the first element on the console. This method has single operation here
+// and take constant amount of time to run that and we represent that using O(1). This is
+// the run time complexity of the method.
+
+const log2 = (arr) => {
+  console.log(arr[0]);
+  console.log(arr[0]);
+};
+
+// If we duplicate the console and print the same element again the run time complexity
+// is going to be same as O(1) because when we are checking the run time complexity we dont
+// really care about the number of operations we just wanna know how much the alogrithm
+// will slow down as input grows larger

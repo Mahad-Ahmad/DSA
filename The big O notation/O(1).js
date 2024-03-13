247ba23e8014afdfe5bcf8c7dffcc36c9f6bfda1
Mahad Ahmad <mahadahmad921@gmail.com>
2024-03-13 09:53:07 +0000

    Explaination for O(n) added

--- The big O notation/O(1).js
+++ The big O notation/O(1).js
@@ -1,7 +1,7 @@
 // O(1)
 
 const log = (arr) => {
-  console.log(arr[0]);
+  console.log(arr[0]); // O(1)
 };
 
 // Here we are consoling the fisrt item of the array no matter how big the array is it
@@ -10,9 +10,10 @@ const log = (arr) => {
 // the run time complexity of the method.
 
 const log2 = (arr) => {
-  console.log(arr[0]);
-  console.log(arr[0]);
+  console.log(arr[0]); // O(1)
+  console.log(arr[0]); // O(1)
 };
+// O(1)
 
 // If we duplicate the console and print the same element again the run time complexity
 // is going to be same as O(1) because when we are checking the run time complexity we dont
---
+++ The big O notation/O(n).js
@@ -0,0 +1,35 @@
+// O(n)
+
+const example = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+};
+// Here we have an array and we are iterating over the items and in this example
+// size of the input matters. As if we have a single item in array we are gonna
+// have one print operations and if we have millions items obviously we are gonna
+// have million print operations so run time cost of this function grows LINEARLY
+// and in direct relation to the size of the input and we represent this as O(n)
+// where n represent the size of the inputs as it grows cost will also grows.
+
+const anotherExample = (arr, names) => {
+    console.log('something'); // O(1)
+  for (let i = 0; i < arr.length; i++) { 
+    console.log(arr[i]); // O(n)
+  }
+
+  for (let j = 0; j < names.length; j++) { 
+    console.log(names[j]); // O(m)
+  }
+  console.log('something'); // O(1)
+};
+// O(1 + n + m + 1) ==> O(2 + n + m)
+
+// In this example we have a run time complexity of O(2 + n + m). When using the 
+// Big O notation we drop the constants because they don't really matters. e.g: if
+// an array has one million in puts adding these 2 operations doesn't really 
+// matters this is not going to add any significant impact on the cost of our algo-
+// rithm as it cost still increases linearly. For n + m our time complexity is dire-
+// ctly proportional to the input so we have to find the complexity and we can 
+// write O(n) instead of O(n + m).
+

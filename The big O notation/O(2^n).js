9b2b9c5f8e9f316684a58962fff78e9d0094657e
Mahad Ahmad <mahadahmad921@gmail.com>
2024-03-14 11:42:53 +0000

    Explaination for O(2^n) added

---
+++ The big O notation/O(2^n).js
@@ -0,0 +1,40 @@
+// O(2 ^ n)
+
+// This is the opposite of the Algorathmic growth as Algorathmic growth slows down
+// as input size grows but Expoential curve grows faster and faster, not scable at
+// all slower then O(n) QUADRATIC curve
+
+
+// O(n) Graph
+//                                             -
+//                                         -
+//                                     -
+//                                 -
+//                             -
+//                         -
+//                     -
+//                 - 
+//             -  
+//          -
+//     -
+// -
+
+// O(2 ^ n) Graph
+//                                      -
+//                                     -
+//                                    -
+//                                   -
+//                                 -
+//                               -
+//                             -
+//                           -
+//                         -
+//                       -
+//                    - 
+//                  -  
+//                -
+//             -
+//           -
+//        -
+//      -
+//   -
